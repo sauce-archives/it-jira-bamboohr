75b9736002747907c1810b993553c93183938a51
Gavin Mogan <gavin@saucelabs.com>
2017-06-04 10:09:47 -0700

    starting cleaning up config more, more borrowing from ac_flask.hipchat

--- ac_flask/__init__.py
+++ ac_flask/__init__.py
@@ -1,8 +1,11 @@
 from flask import jsonify, redirect, request, abort
 from functools import wraps
-import atlassian_jwt
-import jwt
 from jwt.exceptions import DecodeError
+import atlassian_jwt
+import httplib
+import jwt
+import logging
+import os
 import re
 import requests
 
@@ -11,13 +14,18 @@ class ACAddon(object):
     """Atlassian Connect Addon"""
     def __init__(self, app, key, 
                  get_client_by_id_func=None, 
-                 set_client_by_id_func=None):
+                 set_client_by_id_func=None,
+                 config=None,
+                 env_prefix="AC_"):
+        self.app = app
+        self._init_app(app, config, env_prefix)
+
         self.descriptor = {
             "name": "Add bamboohr information to tickets",
             "description": "Add bamboohr information to tickets",
             "key": key,
             "authentication": {"type": "jwt"},
-            "baseUrl": "https://dev.gavinmogan.com",
+            "baseUrl": self._relative_to_base('/'),
             "scopes": ["READ"],
             "vendor": {
                 "name": "Sauce Labs",
@@ -27,11 +35,12 @@ class ACAddon(object):
                 "installed": "/lifecycle/installed",
                 "enabled": "/ping",
             },
+            "links": {
+                "self": self._relative_to_base("/addon/descriptor")
+            },
         }
         self.get_client_by_id = get_client_by_id_func
         self.set_client_by_id = set_client_by_id_func
-
-        self.app = app
 
         class SimpleAuthenticator(atlassian_jwt.Authenticator):
             def get_shared_secret(self, client_key):
@@ -46,6 +55,32 @@ class ACAddon(object):
         @app.route('/addon/descriptor', methods=['GET'])
         def get_descriptor():
             return jsonify(self.descriptor)
+
+    @staticmethod
+    def _init_app(app, config, env_prefix):
+        app.config.from_object('ac_flask.default_settings')
+        if config is not None:
+            app.config.from_object(config)
+
+        if env_prefix is not None:
+            env_vars = {key[len(env_prefix):]: val for key, val in os.environ.items()}
+            app.config.update(env_vars)
+
+        if app.config['DEBUG']:
+            # These two lines enable debugging at httplib level (requests->urllib3->httplib)
+            # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+            # The only thing missing will be the response.body which is not logged.
+            httplib.HTTPConnection.debuglevel = 1
+
+            # You must initialize logging, otherwise you'll not see debug output.
+            logging.basicConfig()
+            logging.getLogger().setLevel(logging.DEBUG)
+            requests_log = logging.getLogger("requests.packages.urllib3")
+            requests_log.setLevel(logging.DEBUG)
+            requests_log.propagate = True
+        else:
+            logging.basicConfig()
+            logging.getLogger().setLevel(logging.WARN)
 
     def _installed_wrapper(self, func):
         def inner(*args, **kwargs):
