4af4b33f73b1d94301be65bc7c8a4da5e17850d0
Gavin Mogan <gavin@saucelabs.com>
2017-06-05 17:57:14 -0700

    tasks cleanup

--- tasks.py
+++ tasks.py
@@ -3,7 +3,11 @@ from invoke import task
 
 @task
 def dev(ctx):
-    ctx.run("FLASK_DEBUG=1 PORT=3000 AC_BASE_URL=https://d39ac125.ngrok.io python web.py")
+    ctx.run("python web.py", env={
+        "FLASK_DEBUG": "1",
+        "PORT": "3000",
+        "AC_BASE_URL": "https://d39ac125.ngrok.io"
+    }, replace_env=False)
 
 
 @task
@@ -19,7 +23,6 @@ def view(ctx):
     print json.dumps([
         c.as_dict() for c in Client.query.all()
     ])
-    # ctx.run("python -mpickle clients.pk")
 
 
 @task
